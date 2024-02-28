bcd821fe3f364573a974325617be1e2df95343bf
Sebastián Benvenuti <sebastian@sbit.com.ar>
2024-02-28 14:00:12 -0300

    xubuntu2310

---
+++ os/xubuntu-23.10-postinstall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Archivo post instalación para Xubuntu 23.10
+# Actualiza la información de los repositorios
+sudo apt update
+
+# Instalar paquetes nuevos sin GUI
+sudo apt install -y ssh wireguard avahi-daemon ethtool wakeonlan \
+    nmon nload iftop iperf axel aria2 curl wget rsync git \
+    bash-completion htop ncdu mc tmux pwgen lm-sensors most bat \
+    lvm2 dosfstools rar unrar unp unzip libemail-outlook-message-perl \
+    mpack ffmpeg imagemagick yubikey-luks
+
+# Instalar paquetes nuevos con GUI
+sudo apt install -y handbrake vlc subtitleeditor zenity \
+    transmission-gtk transmission-remote-gtk filezilla \
+    inkscape nomacs nomacs-l10n simple-scan pdfarranger \
+    yubikey-manager-qt kdeconnect
+
+# Instalar paquetes SNAP
+sudo snap install codium
+
+# Desinstalar paquetes preinstalados
+sudo apt remove -y atril ristretto parole rhythmbox \
+    rhythmbox-plugin-alternative-toolbar rhythmbox-data rhythmbox-plugins
+
+# Limpia todo
+sudo apt autoremove -y
+sudo apt clean
